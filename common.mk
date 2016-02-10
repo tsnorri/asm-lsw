08426f89d8fe4bab5925a70771c106f74918ee53
Tuukka Norri <tuukka.norri@hiit.fi>
2016-02-10 11:41:49 +0200

    Moved common settings to common.mk

--- common.mk
+++ common.mk
@@ -1,14 +1,35 @@
 # Ignore Xcode's setting since the SDK may contain older versions of Clang and libc++.
 unexport SDKROOT
 
-OPT_FLAGS	= -gfull -O0
-#OPT_FLAGS	= -Os
-#OPT_FLAGS	= -O2
+CXX		?= clang++
+GCOVR	?= gcovr
+MKDIR	?= mkdir
 
-CFLAGS		= -c -std=c99 $(OPT_FLAGS) -Wall -Werror -Wno-unused
-CXXFLAGS	= -c -std=c++14 -stdlib=libc++ $(OPT_FLAGS) -Wall -Werror -Wno-unused
-CPPFLAGS	= -I../include $(INCLUDES)
-LDFLAGS		= $(OPT_FLAGS) $(LIBPATHS) -ldivsufsort -lphf -lsdsl
+WARNING_FLAGS	= -Wall -Werror -Wno-unused -Wno-missing-braces
+
+LOCAL_CXXFLAGS		?= -std=c++14
+LOCAL_LDFLAGS		?= -stdlib=libc++
+OPT_FLAGS_DEBUG		?= -ggdb -O0
+OPT_FLAGS_RELEASE	?= -O2
+
+CFLAGS		= -c -std=c99 $(OPT_FLAGS) $(WARNING_FLAGS)
+CXXFLAGS	= -c $(OPT_FLAGS) $(LOCAL_CXXFLAGS) $(WARNING_FLAGS)
+CPPFLAGS	=	-I../include \
+				-I../../lib/bandit \
+				-I../../lib/sdsl/build/include \
+				-I../../lib/sdsl/build/external/libdivsufsort/include \
+				-I../../lib/phf \
+				$(LOCAL_CPPFLAGS)
+LDFLAGS		=	-L../../lib/sdsl/build/lib \
+				-L../../lib/sdsl/build/external/libdivsufsort/lib \
+				-L../../lib/phf \
+				$(OPT_FLAGS) $(LOCAL_LDFLAGS) -ldivsufsort -lphf -lsdsl
+
+ifeq ($(BUILD_STYLE),release)
+	OPT_FLAGS	= $(OPT_FLAGS_RELEASE)
+else
+	OPT_FLAGS	= $(OPT_FLAGS_DEBUG)
+endif
 
 
 %.o: %.cc
